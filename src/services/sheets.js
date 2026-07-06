b607c6d056f628c15a0c8f0d8502dd21a6abbbd4
urijtrufin <yura@gmail.com>
2026-07-06 14:28:57 +0300

    sheets

--- src/services/sheets.js
+++ src/services/sheets.js
@@ -1,5 +1,34 @@
+const { google } = require('googleapis');
+const config = require('../config');
+
+const getClient = async () => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: config.GOOGLE_CREDENTIALS_PATH,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  return auth.getClient();
+};
+
 const saveLead = async (lead) => {
-  console.log('📊 Saving lead:', lead);
+  const client = await getClient();
+  const sheets = google.sheets({ version: 'v4', auth: client });
+
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: config.SPREADSHEET_ID,
+    range: 'Leads!A:E',
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: [[
+        lead.date,
+        lead.name,
+        lead.phone,
+        lead.interest,
+        lead.username,
+      ]],
+    },
+  });
+
+  console.log('📊 Lead saved to Google Sheets:', lead.name);
 };
 
 module.exports = { saveLead };
